d65ea05e577843441583d9435dfdaa5954baebe0
diegomac <diego_esparza02@outlook.com>
2024-08-28 16:01:32 -0600

    use enums

---
+++ day 3/enums.playground/Contents.swift
@@ -0,0 +1,16 @@
+import UIKit
+
+var selected = "Monday"
+selected = "Tuesday"
+selected = "January"
+selected = "Friday "
+
+enum Weekday{
+    case monday, tuesday, wednesday, thursday, friday
+}
+
+var day = Weekday.monday
+day = .tuesday
+day = .friday
+
+
