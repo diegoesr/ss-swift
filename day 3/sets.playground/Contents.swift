c3a688c64e658c04df7c7e85c2dac2453191ef3c
diegomac <diego_esparza02@outlook.com>
2024-08-28 14:54:23 -0600

    set actors

---
+++ day 3/sets.playground/Contents.swift
@@ -0,0 +1,16 @@
+import UIKit
+
+var actors = Set<String>()
+actors.insert("Denzel Washigton")
+actors.insert("Tom Cruise")
+actors.insert("Nicolas Cage")
+actors.insert("Samuel L Jackson")
+print(actors)
+
+var youngersActors = Set<String>()
+youngersActors.insert("Timothee Chalamet")
+youngersActors.insert("Zendaya")
+youngersActors.insert("Cailee Spaeny")
+youngersActors.insert("Sidney Sweeney")
+print(youngersActors)
+
