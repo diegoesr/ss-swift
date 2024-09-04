40ca0342fd9f97ce8a45492645f4778edb77d971
diegomac <diego_esparza02@outlook.com>
2024-09-04 16:46:32 -0600

    day 6

---
+++ day 6/loop.playground/Contents.swift
@@ -0,0 +1,35 @@
+import UIKit
+
+let platforms = ["iOS", "macOS", "tvOS", "watchOS"]
+
+for os in platforms {
+    print("Swift works great on \(os).")
+}
+
+for i in 1...12 {
+    print("The \(i) times table:")
+
+    for j in 1...12 {
+        print("  \(j) x \(i) is \(j * i)")
+    }
+
+    print()
+}
+
+for i in 1...5 {
+    print("Counting from 1 through 5: \(i)")
+}
+
+print()
+
+for i in 1..<5 {
+    print("Counting 1 up to 5: \(i)")
+}
+
+var lyric = "Haters gonna hate´s"
+
+for _ in 1...5 {
+    lyric += " u"
+}
+
+print(lyric)
