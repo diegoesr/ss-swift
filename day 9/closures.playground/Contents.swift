5c8d729f4a34337555c84f6eda4fcc3432042862
diegomac <diego_esparza02@outlook.com>
2024-09-18 14:30:39 -0600

    day 9

---
+++ day 9/closures.playground/Contents.swift
@@ -0,0 +1,57 @@
+import UIKit
+
+func greetUser() {
+    print("Hello there!")
+}
+
+greetUser()
+
+var greetCopy = greetUser
+greetCopy()
+
+
+func getUserData(for id: Int) -> String {
+    if id == 1989 {
+        return "Taylor Swift"
+    } else {
+        return "Anonymous"
+    }
+}
+
+let data: (Int) -> String = getUserData
+let user = data(1989)
+print(user)
+
+let sayHello = { (name: String) -> String in
+    "Hi \(name)!"
+}
+sayHello("Taylor")
+
+
+let team = ["Gloria", "Suzanne", "Piper", "Tiffany", "Tasha"]
+let sortedTeam = team.sorted()
+print(sortedTeam)
+
+func captainFirstSorted(name1: String, name2: String) -> Bool {
+    if name1 == "Suzanne" {
+        return true
+    } else if name2 == "Suzanne" {
+        return false
+    }
+
+    return name1 < name2
+}
+
+//let captainFirstTeam = team.sorted(by: captainFirstSorted)
+//print(captainFirstTeam)
+
+let captainFirstTeam = team.sorted(by: { (name1: String, name2: String) -> Bool in
+    if name1 == "Suzanne" {
+        return true
+    } else if name2 == "Suzanne" {
+        return false
+    }
+
+    return name1 < name2
+})
+print(captainFirstTeam)
---
+++ day 9/trailing_closures.playground/Contents.swift
@@ -0,0 +1,27 @@
+import UIKit
+
+let team = ["Gloria", "Suzanne", "Piper", "Tiffany", "Tasha"]
+
+let captainFirstTeam = team.sorted {
+    if $0 == "Suzanne" {
+        return true
+    } else if $1 == "Suzanne" {
+        return false
+    }
+
+    return $0 < $1
+}
+
+print(captainFirstTeam)
+
+//let reverseTeam = team.sorted {
+    //return $0 > $1
+//}
+
+let reverseTeam = team.sorted { $0 > $1 }
+
+let tOnly = team.filter { $0.hasPrefix("T") }
+print(tOnly)
+
+let uppercaseTeam = team.map { $0.uppercased() }
+print(uppercaseTeam)
