8c82cb936f904b83acfc82a6235c8da128fee6c2
diegomac <diego_esparza02@outlook.com>
2024-10-09 13:45:08 -0600

    update

--- day 4/summary.playground/Contents.swift
+++
@@ -1,9 +0,0 @@
-import UIKit
-//
-// Arrays store many vueles, and read them using indices.
-// Dictionaries store many values, and read them using keys we specify.
-// Sets stores many values, but we don´t choose their order.
-// Enums create our own types to specify a range of acceptable values.
-// Swift uses type inference to figure what data we´re storing.
-
-
